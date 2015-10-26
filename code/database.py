fad7c902888ecd22dc5b378f65e21760cbfd11b9
MuscioCraft <m.hilhorst@hotmail.nl>
2015-10-26 16:05:28 +0100

    hello, you there, is it me you're looking for ?

--- code/database.py
+++ code/database.py
@@ -1,1 +1,8 @@
 __author__ = 'MuscioCraft'
+
+import csv
+
+with open('database.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=';',)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
