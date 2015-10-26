e0a1b0bd508a209560f7ef24c384cda15f23a972
erwindv <erwin.devlieg@student.hu.nl>
2015-10-26 15:12:52 +0100

    test

--- code/hello.py
+++ code/hello.py
@@ -1,2 +1,19 @@
 __author__ = 'alexander'
 print('Hello world')
+
+
+Treintraject = ['Schagen', 'Heerhugowaard', 'Alkmaar', 'Castricum' ,'Zaandam','Amsterdam Sloterdijk', 'Amsterdam Centraal',
+'Amsterdam Amstel','Utrecht Centraal','s-Hertogenbosch','Eindhoven','Weert','Roermond','Sittard','Maastricht']
+
+for i in range (len(Treintraject)):
+    for j in range(i+1, len(Treintraject)):
+        print("Het huidige station is: " + Treintraject[i])
+        print("De resterende stations zijn: "+ str(Treintraject[j:]))
+        break
+    if i+1 == len(Treintraject):
+        print("Het huidige station is: Maastricht")
+        print("Dit is het eindpunt")
+#    else:
+#        print("Het huidige station is: " + str(Treintraject[i]))
+#        print("De resterende stations zijn: " + str(Treintraject[i+1:len(Treintraject)]))
+
