fb28ab9673dd8e4a6801ce1a5fe99340aaceb74a
GiulianoHS <giuliano.hezel@gmail.com>
2015-10-27 15:53:31 +0100

    - Net framework gemaakt

--- code/navigation.py
+++ code/navigation.py
@@ -1,6 +1,7 @@
 __author__ = 'Giuliano'
 
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -9,10 +10,11 @@ class ThuisBioscoop(tk.Tk):
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.iconbitmap(self, default="popcorn.ico")
+
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -43,29 +45,29 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Home", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        poster1 = tk.PhotoImage(file="poster.gif")
+        # poster1 = tk.PhotoImage(file="poster.gif")
 
-        button1 = tk.Button(self, text="The Martian", width=200, height=300, image=poster1,
+        button1 = ttk.Button(self, text="The Martian",
                             command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
-        button2 = tk.Button(self, text="Kill Bill 2", width=200, height=300, image=poster1,
+        button2 = ttk.Button(self, text="Kill Bill 2",
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
-        button3 = tk.Button(self, text="Star Wars: Episode IV",
+        button3 = ttk.Button(self, text="Star Wars: Episode IV",
                             command=lambda: controller.show_frame(PageThree))
         button3.pack()
 
-        button4 = tk.Button(self, text="Limitless",
+        button4 = ttk.Button(self, text="Limitless",
                             command=lambda: controller.show_frame(PageFour))
         button4.pack()
 
-        button5 = tk.Button(self, text="007: Spectre",
+        button5 = ttk.Button(self, text="007: Spectre",
                             command=lambda: controller.show_frame(PageFive))
         button5.pack()
 
-        button6 = tk.Button(self, text="Back to the future",
+        button6 = ttk.Button(self, text="Back to the future",
                             command=lambda: controller.show_frame(PageSix))
         button6.pack()
 
@@ -76,7 +78,7 @@ class PageOne(tk.Frame):
         label = tk.Label(self, text="The Martian", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Terug naar home",
+        button1 = ttk.Button(self, text="Terug naar home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
@@ -88,7 +90,7 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="Kill Bill 2", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Terug naar home",
+        button1 = ttk.Button(self, text="Terug naar home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
@@ -99,7 +101,7 @@ class PageThree(tk.Frame):
         label = tk.Label(self, text="Star wars: Episode IV", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Terug naar home",
+        button1 = ttk.Button(self, text="Terug naar home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
@@ -110,7 +112,7 @@ class PageFour(tk.Frame):
         label = tk.Label(self, text="Limitless", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Terug naar home",
+        button1 = ttk.Button(self, text="Terug naar home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
@@ -121,7 +123,7 @@ class PageFive(tk.Frame):
         label = tk.Label(self, text="007: Spectre", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Terug naar home",
+        button1 = ttk.Button(self, text="Terug naar home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
@@ -132,7 +134,7 @@ class PageSix(tk.Frame):
         label = tk.Label(self, text="Back to the future", font="LARGE_FONT")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Terug naar home",
+        button1 = ttk.Button(self, text="Terug naar home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
