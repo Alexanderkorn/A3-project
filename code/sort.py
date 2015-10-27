3b1c20c087ba155c964d7d7ce7042d0116754e03
MuscioCraft <m.hilhorst@hotmail.nl>
2015-10-27 12:38:30 +0100

    hello, you there, is it me you're looking for ?

---
+++ code/sort.py
@@ -0,0 +1,17 @@
+__author__ = 'Roy'
+import xmltodict
+from xml.dom.minidom import parse
+def read_xml():
+    file = open('data.xml','r')
+    xml_string = file.read()
+    return xmltodict.parse(xml_string)
+
+film_nummer = None
+film_dict = read_xml()
+nodes = parse('data.xml')
+for film_nummer in nodes.getElementsByTagName('film'): # 7 = in range of childnodes
+    #print(film_dict['filmsoptv']['film'][film_nummer]['starttijd'])
+    print(film_nummer.toxml())
+
+
+
