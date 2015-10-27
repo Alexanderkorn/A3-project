6b1a5a990bb82c0cf67b79c51aeaf47c6a14bb0e
MuscioCraft <m.hilhorst@hotmail.nl>
2015-10-27 09:53:04 +0100

    hello, you there, is it me you're looking for ?

--- code/database.py
+++ code/database.py
@@ -1,17 +1,33 @@
 __author__ = 'MuscioCraft'
+import sys
 
 def ticket():
 
+    try:
+        import csv
+        from random import randint
+    except:
+        sys.exit("Er is wat fout gegaan met importeren van de functies.")
 
-    import csv
-    from random import randint
-    ticket = randint(0,999999999)
 
-    with open('database.csv', 'a') as csvfile:
+    try:
+        ticket = randint(0,999999999)
+    except:
+        sys.exit("Er is wat fout gegaan met het ticket nummer aanmaken.")
 
-        writer = csv.writer(csvfile, delimiter=';')
-        writer.writerow([input("Uw naam :"), input("Uw E-mail adres :"), ticket])
 
-    print("Uw ticket nummer is : "+ticket)
+    try:
+        with open('database.csv', 'a') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            writer.writerow([str(input("Uw naam :")).lower(), str(input("Uw E-mail adres :")).lower(), str(input("Welke film ? : (Moet nog een film selcteer menu in(andere opdracht))")).lower() , str(ticket)])
+    except:
+        sys.exit("Er is wat mis gegaan met het openen en of het scrijven van de database")
+
+
+    try:
+        print("Uw ticket nummer is : "+str(ticket))
+    except:
+        sys.exit("Er is wat fout gegaan met het ticket nummer printen.")
+
 
 ticket()
