687c8bc465abd6ca24609069b8ef2fd6443cc881
alexander <alexanderkorn7@gmail.com>
2015-10-27 10:16:13 +0100

    wip

--- code/login.py
+++ code/login.py
@@ -2,8 +2,13 @@ __author__ = 'alexander'
 
 import csv
 
+naambedrijf=input("uw bedrijfs naam:")
 database='database.csv'
 # lijn 3
 def check_aanbieder():
     r=open(database, 'r')
     reader=csv.reader(r, delimiter=';')
+    with open(database, 'r'):
+        for row in reader:
+            if naambedrijf == row[3]: # if the username shall be on column 3 (-> index 2)
+                print("is in file")
