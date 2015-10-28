fad08da5a516dede81218af445800c565a17b354
alexander <alexanderkorn7@gmail.com>
2015-10-28 13:09:21 +0100

    login

--- code/film.py
+++ code/film.py
@@ -6,39 +6,39 @@ import xml.dom.minidom
 import csv
 from xml.dom.minidom import parse
 
-def bedenk_maar_even_wat(): 
-    def read_xml():
-        file = open('data.xml','r')
-        xml_string = file.read()
-        return xmltodict.parse(xml_string)
 
-    film_nummer = None
-    film_dict = read_xml()
-    nodes = parse('data.xml')
+def read_xml():
+    file = open('data.xml','r')
+    xml_string = file.read()
+    return xmltodict.parse(xml_string)
 
-    for film_nummer in nodes.getElementsByTagName('film'):
+film_nummer = None
+film_dict = read_xml()
+nodes = parse('data.xml')
 
-        document = film_nummer.toxml()
-        dom = xml.dom.minidom.parseString(document)
+for film_nummer in nodes.getElementsByTagName('film'):
 
-        def getText(nodelist):
-            rc = []
-            for node in nodelist:
-                if node.nodeType == node.TEXT_NODE:
-                    rc.append(node.data)
-            return ''.join(rc)
+    document = film_nummer.toxml()
+    dom = xml.dom.minidom.parseString(document)
 
-        def handleTok(tokenlist):
-            texts = ""
-            for token in tokenlist:
-                texts += ""+ getText(token.childNodes)
-            return texts
-        foo = dom.getElementsByTagName("zender")
-        text = handleTok(foo)
+    def getText(nodelist):
+        rc = []
+        for node in nodelist:
+            if node.nodeType == node.TEXT_NODE:
+                rc.append(node.data)
+        return ''.join(rc)
 
-        bob = dom.getElementsByTagName("titel")
-        text2 = handleTok(bob)
-        x = "RTL7"
-        if x in text:
-            print(text, text2)
+    def handleTok(tokenlist):
+        texts = ""
+        for token in tokenlist:
+            texts += ""+ getText(token.childNodes)
+        return texts
+    foo = dom.getElementsByTagName("zender")
+    text = handleTok(foo)
+
+    bob = dom.getElementsByTagName("titel")
+    text2 = handleTok(bob)
+    x = "RTL7"
+    if x in text:
+        print(text, text2,"laol")
 
