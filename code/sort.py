87ecae55fabf008352d018c445be8d1479884cce
MuscioCraft <m.hilhorst@hotmail.nl>
2015-10-28 10:51:18 +0100

    hello, you there, is it me you're looking for ?

--- code/sort.py
+++ code/sort.py
@@ -1,6 +1,11 @@
 __author__ = 'Roy'
+#bron: http://stackoverflow.com/questions/6653128/getting-text-between-xml-tags-with-minidom
+
 import xmltodict
+import xml.dom.minidom
+import csv
 from xml.dom.minidom import parse
+
 def read_xml():
     file = open('data.xml','r')
     xml_string = file.read()
@@ -9,9 +14,47 @@ def read_xml():
 film_nummer = None
 film_dict = read_xml()
 nodes = parse('data.xml')
-for film_nummer in nodes.getElementsByTagName('film'): # 7 = in range of childnodes
-    #print(film_dict['filmsoptv']['film'][film_nummer]['starttijd'])
-    print(film_nummer.toxml())
+
+for film_nummer in nodes.getElementsByTagName('film'):
+
+    document = film_nummer.toxml()
+    dom = xml.dom.minidom.parseString(document)
+
+    def getText(nodelist):
+        rc = []
+        for node in nodelist:
+            if node.nodeType == node.TEXT_NODE:
+                rc.append(node.data)
+        return ''.join(rc)
+
+    def handleTok(tokenlist):
+        texts = ""
+        for token in tokenlist:
+            texts += ""+ getText(token.childNodes)
+        return texts
+    foo = dom.getElementsByTagName("starttijd")
+    text = handleTok(foo)
+
+    bob = dom.getElementsByTagName("titel")
+    text2 = handleTok(bob)
+    print(text, text2)
 
 
+
+
+def achternamen():
+    f=open('database.csv','r')
+    reader=csv.reader(f, delimiter=';')
+    x=0
+    for row in reader:
+        if x == 0:
+            x=1
+            pass
+        else:
+            a = row[1]
+            print(sorted(reader))
+
+    f.close()
+
+achternamen()
 
