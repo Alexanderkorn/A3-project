14f067d628bec367eb7cdefc2d9714acf27166de
MuscioCraft <m.hilhorst@hotmail.nl>
2015-10-28 13:04:05 +0100

    hello, you there, is it me you're looking for ?

---
+++ code/film.py
@@ -0,0 +1,44 @@
+__author__ = 'MuscioCraft'
+#bron: http://stackoverflow.com/questions/6653128/getting-text-between-xml-tags-with-minidom
+
+import xmltodict
+import xml.dom.minidom
+import csv
+from xml.dom.minidom import parse
+
+def bedenk_maar_even_wat(): 
+    def read_xml():
+        file = open('data.xml','r')
+        xml_string = file.read()
+        return xmltodict.parse(xml_string)
+
+    film_nummer = None
+    film_dict = read_xml()
+    nodes = parse('data.xml')
+
+    for film_nummer in nodes.getElementsByTagName('film'):
+
+        document = film_nummer.toxml()
+        dom = xml.dom.minidom.parseString(document)
+
+        def getText(nodelist):
+            rc = []
+            for node in nodelist:
+                if node.nodeType == node.TEXT_NODE:
+                    rc.append(node.data)
+            return ''.join(rc)
+
+        def handleTok(tokenlist):
+            texts = ""
+            for token in tokenlist:
+                texts += ""+ getText(token.childNodes)
+            return texts
+        foo = dom.getElementsByTagName("zender")
+        text = handleTok(foo)
+
+        bob = dom.getElementsByTagName("titel")
+        text2 = handleTok(bob)
+        x = "RTL7"
+        if x in text:
+            print(text, text2)
+
