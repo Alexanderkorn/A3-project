a4d91573e82b1fafc20b8dafc24d9691b066cfec
Stijn <secvergouwen@gmail.com>
2015-10-29 16:47:02 +0100

    test

---
+++ code/Giuliano_lijst2.py
@@ -0,0 +1,83 @@
+__author__ = 'Stijn'
+__author__ = 'Stijn'
+import tkinter as tk
+from tkinter import ttk
+
+class ThuisBioscoop(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Home", font="LARGE_FONT")
+        label.pack(pady=10, padx=10)
+
+        igm = tk.PhotoImage(file="./poster.gif")
+
+        button1 = ttk.Button(self, text="The Martian", image=igm,
+                            command=lambda: controller.show_frame(PageOne))
+        button1.pack()
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="The Martian", font="LARGE_FONT")
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(self, text="Terug naar home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        verder_button = ttk.Button(self, text="Verder", command=(lambda: None))
+        verder_button.pack()
+
+        leverancier_lijst = tk.Listbox(self)
+        f = ["Piet", "Jan", "Hendrik", "Hugo"]
+
+        for i in f:
+            leverancier_lijst.insert(tk.END, i)
+
+        # def nummer_leverancier():
+        #     goede_nummer_leverancier = str(leverancier_lijst.curselection())
+        #     index_goede_nummer_leverancier = int(goede_nummer_leverancier[1])
+        #     global zender_leverancier
+        #     zender_leverancier = zenders[index_goede_nummer_leverancier]
+        #     # aan het eind van de functie gaat hij door naar het nieuwe scherm -->
+        #     self.Weergave_van_eigen_films()
+
+        # je hebt een button nodig in het scherm
+        # de button voort de functie hierboven uit
+
+        leverancier_lijst.pack()
+
+
+app = ThuisBioscoop()
+app.mainloop()
